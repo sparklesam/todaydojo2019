3a4f5f523844e0732c7f3e51ecd5da33ba9b16e5
Samuel Wong <samuel@playa.hk>
2019-05-21 16:35:29 +0800

    Fix Build Error

--- gatsby-config.js
+++ gatsby-config.js
@@ -175,7 +175,7 @@ module.exports = {
             serialize(ctx) {
               const { rssMetadata } = ctx.query.site.siteMetadata;
               return ctx.query.allPrismicPost.edges.map(edge => ({
-                categories: edge.node.data.categories.category,
+                categories: edge.node.data.categories
                 date: edge.node.data.date,
                 title: edge.node.data.title.text,
                 author: "Samuel Wong",

189240d4d57d75b0790f3e458a045bc0ce39a71d
Samuel Wong <samuel@playa.hk>
2019-06-07 17:57:57 +0800

    Add New Sitemap & Update Gatsby to Latest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,37 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
 });
 
-const { RichText } = require('prismic-reactjs');
+const { RichText } = require("prismic-reactjs");
 
 // We don't want to import every PrismJS component - so that's why they're required individually
-const Prism = require('prismjs');
-require('prismjs/components/prism-javascript');
-require('prismjs/components/prism-css');
-require('prismjs/components/prism-scss');
-require('prismjs/components/prism-jsx');
-require('prismjs/components/prism-bash');
-require('prismjs/components/prism-json');
-require('prismjs/components/prism-diff');
-require('prismjs/components/prism-markdown');
-require('prismjs/components/prism-graphql');
+const Prism = require("prismjs");
+require("prismjs/components/prism-javascript");
+require("prismjs/components/prism-css");
+require("prismjs/components/prism-scss");
+require("prismjs/components/prism-jsx");
+require("prismjs/components/prism-bash");
+require("prismjs/components/prism-json");
+require("prismjs/components/prism-diff");
+require("prismjs/components/prism-markdown");
+require("prismjs/components/prism-graphql");
 
 const { Elements } = RichText;
 
 // Labels with this name will be inline code
-const codeInline = ['text'];
+const codeInline = ["text"];
 // Labels with these names will become code blocks
-const codeBlock = ['javascript', 'css', 'scss', 'jsx', 'bash', 'json', 'diff', 'markdown', 'graphql'];
+const codeBlock = [
+  "javascript",
+  "css",
+  "scss",
+  "jsx",
+  "bash",
+  "json",
+  "diff",
+  "markdown",
+  "graphql"
+];
 
 const {
   _pathPrefix,
@@ -37,8 +47,8 @@ const {
   logo,
   favicon,
   siteLanguage,
-  twitter,
-} = require('./config/website');
+  twitter
+} = require("./config/website");
 
 module.exports = {
   /* General Information */
@@ -56,17 +66,18 @@ module.exports = {
     description,
     keyword,
     banner: logo,
-    twitter,
+    twitter
   },
   /* Plugins */
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-emotion',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-emotion",
     {
-      resolve: 'gatsby-source-prismic',
+      resolve: "gatsby-source-prismic",
       options: {
-        repositoryName: 'dojo2',
-        accessToken: 'MC5XOHluMmhJQUFDa0FuWGJG.77-977-9en9zZTrvv73vv73vv73vv73vv71y77-977-9Ue-_vT3vv73vv73vv73vv71I77-9BO-_ve-_vXESMO-_vXw',
+        repositoryName: "dojo2",
+        accessToken:
+          "MC5XOHluMmhJQUFDa0FuWGJG.77-977-9en9zZTrvv73vv73vv73vv73vv71y77-977-9Ue-_vT3vv73vv73vv73vv71I77-9BO-_ve-_vXESMO-_vXw",
         linkResolver: () => post => `/${post.uid}`,
         htmlSerializer: () => (type, element, content) => {
           switch (type) {
@@ -74,26 +85,36 @@ module.exports = {
             case Elements.label: {
               // Use the inline code for labels that are in the array of "codeInline"
               if (codeInline.includes(element.data.label)) {
-                return `<code class="language-${element.data.label}">${content}</code>`;
+                return `<code class="language-${
+                  element.data.label
+                }">${content}</code>`;
               }
               // Use the blockquote for labels with the name "quote"
-              if (element.data.label === 'quote') {
+              if (element.data.label === "quote") {
                 return `<blockquote><p>${content}</p></blockquote>`;
               }
               // Use the code block for labels that are in the array of "codeBlock"
               // Choose the right PrismJS highlighting with the label name
               if (codeBlock.includes(element.data.label)) {
-                return `<pre class="language-${element.data.label}"><code class="language-${
+                return `<pre class="language-${
                   element.data.label
-                }">${Prism.highlight(content, Prism.languages[element.label])}</code></pre>`;
+                }"><code class="language-${
+                  element.data.label
+                }">${Prism.highlight(
+                  content,
+                  Prism.languages[element.label]
+                )}</code></pre>`;
               }
               return null;
             }
             case Elements.preformatted: {
               if (codeBlock.includes(element.label)) {
-                return `<pre class="language-${element.label}"><code class="language-${
+                return `<pre class="language-${
                   element.label
-                }">${Prism.highlight(element.text, Prism.languages[element.label])}</code></pre>`;
+                }"><code class="language-${element.label}">${Prism.highlight(
+                  element.text,
+                  Prism.languages[element.label]
+                )}</code></pre>`;
               }
               return null;
             }
@@ -101,29 +122,29 @@ module.exports = {
               return null;
             }
           }
-        },
-      },
+        }
+      }
     },
-    'gatsby-plugin-lodash',
+    "gatsby-plugin-lodash",
     // Although this starter doesn't use local files this plugin is necessary for the gatsby-image features of gatsby-source-prismic
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: 'src',
-        path: `${__dirname}/src/`,
-      },
+        name: "src",
+        path: `${__dirname}/src/`
+      }
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-plugin-typography',
+      resolve: "gatsby-plugin-typography",
       options: {
-        pathToConfigModule: 'config/typography.js',
-      },
+        pathToConfigModule: "config/typography.js"
+      }
     },
-    'gatsby-plugin-sitemap',
+    "gatsby-plugin-advanced-sitemap",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
         name: _title,
         short_name: _titleAlt,
@@ -131,13 +152,13 @@ module.exports = {
         start_url: _pathPrefix,
         background_color: backgroundColor,
         theme_color: themeColor,
-        display: 'standalone',
-        icon: favicon,
-      },
+        display: "standalone",
+        icon: favicon
+      }
     },
     // Must be placed at the end
-    'gatsby-plugin-offline',
-    'gatsby-plugin-netlify',
+    "gatsby-plugin-offline",
+    "gatsby-plugin-netlify",
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
@@ -149,8 +170,8 @@ module.exports = {
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-      },
+        exclude: ["/preview/**", "/do-not-track/me/too/"]
+      }
     },
     {
       resolve: "gatsby-plugin-feed",
@@ -181,7 +202,7 @@ module.exports = {
                 title: edge.node.data.title.text,
                 author: "Samuel Wong",
                 url: "https://pins.desktopofsamuel.com/" + edge.node.uid,
-                guid: "https://pins.desktopofsamuel.com/" + edge.node.uid,
+                guid: "https://pins.desktopofsamuel.com/" + edge.node.uid
               }));
             },
             query: `
@@ -233,10 +254,10 @@ module.exports = {
             }
           `,
             output: "/rss.xml",
-            title: "Your Site's RSS Feed",
+            title: "Your Site's RSS Feed"
           }
         ]
-      },
-    },
-  ],
+      }
+    }
+  ]
 };
